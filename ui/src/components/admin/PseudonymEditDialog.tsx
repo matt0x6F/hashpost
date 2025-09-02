bff92b63683db664c292c8f5dfc3b0d2eb5c4a00
Matt Ouille <matt.ouille@protonmail.com>
2025-09-01 23:35:40 -0700

    Fix linting error

--- ui/src/components/admin/PseudonymEditDialog.tsx
+++ ui/src/components/admin/PseudonymEditDialog.tsx
@@ -189,7 +189,7 @@ export function PseudonymEditDialog({
     }
   };
 
-  const handleInputChange = (field: string, value: any) => {
+  const handleInputChange = (field: string, value: string | boolean) => {
     setFormData(prev => ({ ...prev, [field]: value }));
   };
 
